6425f877dae4cf740c5d573bf4b70e6e457c8594
pbishal <pbishal60@gmail.com>
2021-04-16 01:19:50 +0530

    Last name added

--- UserRegistration/Patterns.cs
+++ UserRegistration/Patterns.cs
@@ -8,9 +8,14 @@ namespace UserRegistration
     class Patterns
     {
         public static string REGEX_FIRSTNAME= "^[A-Z][a-z]{2,}$";
+        public static string REGEX_LASTNAME= "^[A-Z][a-z]{2,}$";
         public bool ValidateFirstName(string firstName)
         {
             return Regex.IsMatch(firstName, REGEX_FIRSTNAME);
+        }
+        public bool ValidateLastName(string lastName)
+        {
+            return Regex.IsMatch(lastName, REGEX_FIRSTNAME);
         }
     }
 }
--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -9,7 +9,10 @@ namespace UserRegistration
         {
             Patterns patterns = new Patterns();
             Console.WriteLine("enter the first name:");
-            Console.WriteLine(patterns.ValidateFirstName(Console.ReadLine()));        
+            Console.WriteLine(patterns.ValidateFirstName(Console.ReadLine()));
+
+            Console.WriteLine("enter the last name:");
+            Console.WriteLine(patterns.ValidateFirstName(Console.ReadLine()));
         }
     }
 }
