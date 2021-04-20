a7e0c19062d527df25e43471c001aada70540003
pbishal <pbishal60@gmail.com>
2021-04-20 22:28:24 +0530

    Rule 2 added

--- UserRegistration/Patterns.cs
+++ UserRegistration/Patterns.cs
@@ -11,8 +11,8 @@ namespace UserRegistration
         public static string REGEX_LASTNAME= "^[A-Z][a-z]{3,}$";
         public static string REGEX_MOBILENUMBER = "^[0-9]{2}[ ][0-9]{10}$";
         public static string REGEX_EMAIL = @"^[a-z]{3,}\.[a-z0-9]{2,}\@[a-z]{2,}\.[a-z]{2}\.[a-z]{2}";
-        public static string REGEX_PASSWORD = "[A-Za-z]{8}";
-        public bool ValidateFirstName(string firstName)
+        public static string REGEX_PASSWORD = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$";
+    public bool ValidateFirstName(string firstName)
         {
             return Regex.IsMatch(firstName, REGEX_FIRSTNAME);
         }
--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -8,17 +8,17 @@ namespace UserRegistration
         static void Main(string[] args)
         {
             Patterns patterns = new Patterns();
-            Console.WriteLine("enter the first name:");
-            Console.WriteLine(patterns.ValidateFirstName(Console.ReadLine()));
+            //Console.WriteLine("enter the first name:");
+            //Console.WriteLine(patterns.ValidateFirstName(Console.ReadLine()));
 
-            Console.WriteLine("enter the last name:");
-            Console.WriteLine(patterns.ValidateFirstName(Console.ReadLine()));
+            //Console.WriteLine("enter the last name:");
+            //Console.WriteLine(patterns.ValidateFirstName(Console.ReadLine()));
 
-            Console.WriteLine("enter the mobile number:");
-            Console.WriteLine(patterns.ValidateMobileNumber(Console.ReadLine()));
+            //Console.WriteLine("enter the mobile number:");
+            //Console.WriteLine(patterns.ValidateMobileNumber(Console.ReadLine()));
 
-            Console.WriteLine("enter the email:");
-            Console.WriteLine(patterns.ValidateEmail(Console.ReadLine()));
+            //Console.WriteLine("enter the email:");
+            //Console.WriteLine(patterns.ValidateEmail(Console.ReadLine()));
 
             Console.WriteLine("enter the password:");
             Console.WriteLine(patterns.ValidatePassword(Console.ReadLine()));
